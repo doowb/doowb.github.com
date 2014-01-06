ddbf68ccaffb7959a34b6110f5388d7a9aadcd5a
Brian Woodward <brian.woodward@gmail.com>
2014-01-06 01:28:26 -0500

    adding a starter for loading partials

---
+++ src/plugins/assemble-plugin-partials.js
@@ -0,0 +1,71 @@
+/*
+ * Assemble Plugin: assemble-plugin-partials
+ * https://github.com/doowb/assemble-plugin-partials
+ * Assemble is the 100% JavaScript static site generator for Node.js, Grunt.js, and Yeoman.
+ *
+ * Copyright (c) 2014 doowb
+ * Licensed under the MIT license.
+ */
+
+var utils = require('assemble').utils;
+var es = require('event-stream');
+var fs = require('vinyl-fs');
+var async = require('async');
+var path = require('path');
+
+var options = {
+  stages: [
+    utils.plugins.stages.assembleBeforePartials,
+    utils.plugins.stages.assembleAfterPartials
+  ]
+};
+
+var callbackStream = function (callback) {
+  return function end() {
+    this.emit('end');
+    callback();
+  };
+};
+
+var plugin = module.exports = function (assemble) {
+
+  assemble.registerPlugin(
+    'assemble-plugin-partials',
+    'Load partials and save any yaml front matter.',
+    options,
+    function (params, done) {
+
+      switch (params.stage) {
+      case utils.plugins.stages.assembleBeforePartials:
+
+        this.partials = this.partials || {};
+
+        var saveFile = function (file, done) {
+          var name = path.basename(file.path, path.extname(file.path));
+          this.partials[name] = {};
+          this.partials[name].raw = file.contents.toString();
+          done(null, file);
+        }.bind(this);
+
+        async.series(
+            [
+
+              function (next) {
+              fs.src(this.options.partials)
+                .pipe(es.map(saveFile))
+                .pipe(es.through(null, callbackStream(next)))
+              }.bind(this)
+            ],
+          done);
+        break;
+
+      case utils.plugins.stages.assembleAfterPartials:
+        console.log('this.partials', this.partials);
+        done();
+        break;
+      };
+
+    }
+  );
+
+};
